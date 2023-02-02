a4009f0e6710a715cf5d237741f7b33b94056deb
Filb68 <122360142+Filb68@users.noreply.github.com>
2023-02-03 00:18:01 +0100

    Add files via upload

---
+++ lib/player.rb
@@ -0,0 +1,13 @@
+class Player
+
+    attr_accessor :player_one_name, :player_two_name
+
+    def initialize
+        @player_one_name = player_one_name
+        puts "joueur 1 - Entre ton nom"
+        @player_one_name = gets.chomp
+    end
+
+
+
+end
